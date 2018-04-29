03e1d823c947ca17d1e0bcbca869c3ffda6ce443
NLGRF <nongrace6654@gmail.com>
2018-04-30 02:35:17 +0700

    2 Way Data Binding TextBox

--- app.js
+++ app.js
@@ -3,29 +3,9 @@ new Vue({
   data:{
     name:"Nonpavit Detbun",
     job:"Front-End Developer",
-    age:22,
-    status:false,
-    count:0,
     github: 'https://github.com/NLGRF'
   },
   methods: {
-    getName:function() {
-      return this.name
-    },
-    setName:function(n) {
-      return this.name=n
-    },
-    addAge:function() {
-      this.age++;
-      console.log(this.age);
-    },
-    subtractAge:function(){
-      this.age--;
-      console.log(this.age);
-    },
-    addCount:function(){
-      this.count++;
-      console.log(this.count);
-    }
+
   }
 })
