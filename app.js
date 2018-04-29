96e087624e15495e524922217ac855ae4720d729
NLGRF <nongrace6654@gmail.com>
2018-04-30 02:00:13 +0700

    Data & Method

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 new Vue({
   el:"#vue-app",
   data:{
-    message:"Hello World"
+    name:"Nonpavit Detbun",
+    job:"Front-End Developer",
+    age:22,
+    status:false
+  },
+  methods: {
+    getName:function() {
+      return this.name
+    },
+    setName:function(n) {
+      return this.name=n
+    }
   }
 })
