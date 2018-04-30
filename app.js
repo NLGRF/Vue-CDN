0a44293cf78975fee365aa9f2481a436ae09c8b9
NLGRF <nongrace6654@gmail.com>
2018-04-30 14:48:36 +0700

    Loop v-for

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 new Vue({
   el:"#vue-app",
   data:{
-    like:false
+    like:false,
+    product:['ผงซักฟอก','แปรงสีฟัน','สบู่','ผ้าขนหนู','กางเกง','เสื้อ'],
+    employee:[
+      {name:'User1',age:22,status:false},
+      {name:'User2',age:27,status:true}
+    ]
   },
   methods: {
 
