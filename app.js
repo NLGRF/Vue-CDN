c528ffdd153a32af9f0993c24a9348d1cca937f2
NLGRF <nongrace6654@gmail.com>
2018-05-01 00:21:21 +0700

    Props Validate

--- app.js
+++ app.js
@@ -12,6 +12,20 @@ Vue.component('Post',{
   template:'<h3>{{title}}</h3>'
 });
 
+Vue.component('Showview',{
+  props:{
+    title:{
+        type:String,
+        required:true
+    },
+    view:{
+        type:Number,
+        default:0
+    }
+  },
+  template:'<h3>{{title}} | {{view}}</h3>'
+});
+
 new Vue({
   el:"#vue-app"
 })
