3d1b56fa856ca1d1f556c7b0470cf3b6e96a37a9
NLGRF <nongrace6654@gmail.com>
2018-04-30 23:56:20 +0700

    Multiple Vue

--- app.js
+++ app.js
@@ -1,60 +1,26 @@
-new Vue({
-  el:"#vue-app",
+var post = new Vue({
+    el:"#post",
+    data:{
+      title:'Project Name',
+      message:'Project Details'
+    }
+});
+
+var comment = new Vue({
+  el:"#comment",
   data:{
-    newData:{
-      empName:'',
-      salary:0
-    },
-    empGroup: [
-      {
-        empName: "Abc",
-        salary:5000
-      },
-      {
-        empName: "Efg",
-        salary:3000
-      }
-    ]
+    text:'อากาศดีมาก'
+  }
+});
+
+var likeSystem = new Vue({
+  el:"#likeSystem",
+  data:{
+    like:false
   },
   methods: {
-    addEmp:function() {
-      this.empGroup.push({
-        empName: this.newData.empName,
-        salary: this.newData.salary
-      });
-      this.newData.empName='';
-      this.newData.salary=0;
-    },
-    showMessage:function(){
-      // console.log("บันทึกข้อมูลเรียบร้อย");
-      alert("Complete");
-      console.log("Send Data...");
+    changeLike:function() {
+      console.log("like");
     }
-  },
-  computed:{
-     // แยกมาจัดการด้านการคำนวณ
-      summation:function(){
-        var sum = this.empGroup.reduce(function(value, data) {
-          return value + Number(data.salary);
-          // 0+5000 = 5000
-          // 5000+4000 = 9000
-        }, 0);
-        return sum;
-      },
-      avg:function(){
-        var sum = this.empGroup.reduce(function(value, data) {
-          return value + Number(data.salary);
-          // 0+5000 = 5000
-          // 5000+4000 = 9000
-        }, 0);
-        return sum/this.empGroup.length;
-      }
-    },
-    watch: {
-      //หลังจากที่ summation ทำงานเสร็จ
-      summation:function() {
-        //ทำอะไร
-        this.showMessage();
-      }
-    }
+  }
 });
