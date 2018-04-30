8cb90db3207fe3906ca23eb68d5a170d0e8c0541
NLGRF <nongrace6654@gmail.com>
2018-05-01 00:49:31 +0700

    Slots

--- app.js
+++ app.js
@@ -1,48 +1,10 @@
-Vue.component('Non',{
-  data:function(){
-    return{
-      count:0
-    }
-  },
-  template:'<button v-on:click="count++">Clike : {{count}}</button>'
-});
-
-Vue.component('Post',{
-  props: ['title'],
-  template:'<h3>{{title}}</h3>'
-});
-
-Vue.component('Showview',{
-  props:{
-    title:{
-        type:String,
-        required:true
-    },
-    view:{
-        type:Number,
-        default:0
-    }
-  },
-  template:'<h3>{{title}} | {{view}}</h3>'
-});
-
-Vue.component('Showcomment',{
-  props: ['commentpost'],
-  template:'<li><i>{{commentpost}}</i></li>'
-})
+var food={
+  template:'#foodarea'
+};
 
 new Vue({
   el:"#vue-app",
-  data:{
-    newComment:'',
-    comments:[
-      'AAA','BBB','CCC','DDD'
-    ]
-  },
-  methods: {
-    addComment:function() {
-      this.comments.push(this.newComment)
-      this.newComment=''
-    }
+  components: {
+    foodApp:food
   }
 })
